fa88610a26b1848b06d223c23f0c10c6d49d0c65
Linh Nguyễn <30436084+BeBuNNL@users.noreply.github.com>
2019-07-11 16:42:00 +0700

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'firstProj'`, () => {
+  it(`should have as title 'fAp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('firstProj');
+    expect(app.title).toEqual('fAp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to firstProj!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to fAp!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup,  FormControl, AbstractControl,  Validators, ValidationErrors } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-export class AppComponent implements OnInit {
-  title = 'Login Form';
-  
-  fLogin: FormGroup;
-  emailInp: FormControl;
-  pwdInp: FormControl;
-  constructor(){}
-
-  ngOnInit(){
-    this.emailInp = new FormControl('', Validators.compose([
-      Validators.required,
-      //Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')//Simple but after @ 3 character => alway true
-      //Validators.pattern("^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")//W3C, same
-      Validators.pattern("[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-
-    ]));
-    this.pwdInp = new FormControl('', Validators.compose([
-      Validators.required,
-      Validators.minLength(6)
-    ]));
-    this.fLogin = new FormGroup({
-      email: this.emailInp,
-      password: this.pwdInp
-    });
-    new FormControl('', {updateOn:'blur'})
-  }
-
-  get emailAdr(){
-    return this.fLogin.get('email')
-  }
-
-  onSubmit(){
-    console.log(this.fLogin);
-  }
-
-  getFormControl(name){
-    return this.fLogin.get(name);
-  }
+export class AppComponent {
+  title = 'fAp';
 }
